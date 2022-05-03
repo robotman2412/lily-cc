93a9fdb6b86e3caaa62a2ed2ac2a82a8718f5b46
RobotMan2412 <julian@scheffers.net>
2022-05-03 12:18:07 +0200

    MOAR TEST CASES!

---
+++ test/test_errors.c
@@ -0,0 +1,33 @@
+
+int func() {
+	// Should error.
+	if (undefinedvar) {
+		return 1;
+	}
+}
+
+int next() {
+	int q;
+	// Should error.
+	192 = q;
+	3 <<= q;
+	// Shouldn't error.
+	*192 = 2;
+	1[2] = 3;
+}
+
+void call() {
+	// Should warn implicit declaration.
+	undefinedfunc();
+	// Shouldn't warn.
+	next();
+}
+
+// Duplicate definition of call().
+void call();
+
+void arg_error(int q);
+void arg_error();
+void arg_error(int q, int p) {
+	
+}

662a0e22fa5f0741112c11698824de4c61a99631
RobotMan2412 <julian@scheffers.net>
2022-05-14 20:37:35 +0200

    Some objects, work of.

--- src/objects/objects.c
+++ src/objects/objects.c
@@ -1,2 +1,12 @@
 
+#include <asm_postproc.h>
 #include <objects.h>
+#include <stdio.h>
+
+void output_logisim_hex(asm_ctx_t *ctx) {
+	FILE *loop = fopen("/tmp/lilly-cc-tmp-bin", "w+");
+	FILE *real = ctx->out_fd;
+	ctx->out_fd = loop;
+	output_native(ctx);
+	
+}

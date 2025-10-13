2e32f7f31865a2bf2b8dde11207e6944252e96b5
RobotMan2412 <julian@scheffers.net>
2025-10-13 12:30:21 +0200

    Add enum test

--- src/test/front/c/c_compiler_test.c
+++ src/test/front/c/c_compiler_test.c
@@ -5,7 +5,13 @@
 
 #include "c_compiler.h"
 #include "c_parser.h"
+#include "ir.h"
+#include "ir_optimizer.h"
+#include "ir_types.h"
+#include "list.h"
 #include "testcase.h"
+
+#include <string.h>
 
 
 
@@ -228,3 +234,100 @@ static char *test_c_compile_func() {
     return TEST_OK;
 }
 LILY_TEST_CASE(test_c_compile_func)
+
+
+static char *test_c_compile_enum() {
+    // clang-format off
+    char const source[] =
+    "enum a {\n"
+    "    a_0,\n"
+    "    a_1 = 9,\n"
+    "};\n"
+    "enum a enumtest() {\n"
+    "    return a_1;\n"
+    "}\n"
+    ;
+    // clang-format on
+    cctx_t       *cctx = cctx_create();
+    srcfile_t    *src  = srcfile_create(cctx, "<test_c_compile_enum>", source, sizeof(source) - 1);
+    tokenizer_t  *tctx = c_tkn_create(src, C_STD_def);
+    c_parser_t    pctx = {.tkn_ctx = tctx, .type_names = STR_SET_EMPTY};
+    c_compiler_t *cc   = c_compiler_create(
+        cctx,
+        (c_options_t){
+              .c_std          = C_STD_def,
+              .char_is_signed = true,
+              .short16        = true,
+              .int32          = true,
+              .long64         = true,
+              .size_type      = C_PRIM_ULONG,
+        }
+    );
+
+    // Parse the enum.
+    token_t enum_tok = c_parse_decls(&pctx, true);
+
+    if (cctx->diagnostics.len) {
+        diagnostic_t const *diag = (diagnostic_t const *)cctx->diagnostics.head;
+        printf("\n");
+        while (diag) {
+            print_diagnostic(diag, stderr);
+            diag = (diagnostic_t const *)diag->node.next;
+        }
+        c_tkn_debug_print(enum_tok);
+        return TEST_FAIL;
+    }
+
+    // Parse the function.
+    token_t func_tok = c_parse_decls(&pctx, true);
+
+    if (cctx->diagnostics.len) {
+        diagnostic_t const *diag = (diagnostic_t const *)cctx->diagnostics.head;
+        printf("\n");
+        while (diag) {
+            print_diagnostic(diag, stderr);
+            diag = (diagnostic_t const *)diag->node.next;
+        }
+        c_tkn_debug_print(func_tok);
+        return TEST_FAIL;
+    }
+
+    // Compile both enum and function.
+    c_compile_decls(cc, NULL, NULL, &cc->global_scope, &enum_tok);
+    c_prepass_t prepass = c_precompile_pass(&func_tok);
+    ir_func_t  *func    = c_compile_func_def(cc, &func_tok, &prepass);
+    c_prepass_destroy(prepass);
+
+    if (cctx->diagnostics.len) {
+        diagnostic_t const *diag = (diagnostic_t const *)cctx->diagnostics.head;
+        printf("\n");
+        while (diag) {
+            print_diagnostic(diag, stderr);
+            diag = (diagnostic_t const *)diag->node.next;
+        }
+        ir_func_serialize(func, NULL, stdout);
+        return TEST_FAIL;
+    }
+
+    // Assert that the function is a simple one that literally returns s32'9
+    ir_func_to_ssa(func);
+    ir_optimize(func);
+    EXPECT_INT(func->code_list.len, 1);
+    ir_code_t const *code = func->entry;
+    EXPECT_INT(code->insns.len, 1);
+    ir_insn_t const *insn = container_of(code->insns.head, ir_insn_t, node);
+    EXPECT_INT(insn->type, IR_INSN_RETURN);
+    EXPECT_INT(insn->operands[0].type, IR_OPERAND_TYPE_CONST);
+    EXPECT_INT(insn->operands[0].iconst.prim_type, IR_PRIM_s32);
+    EXPECT_INT(insn->operands[0].iconst.constl, 9);
+
+    ir_func_delete(func);
+    tkn_delete(func_tok);
+    tkn_delete(enum_tok);
+
+    c_compiler_destroy(cc);
+    tkn_ctx_delete(tctx);
+    cctx_delete(cctx);
+    return TEST_OK;
+}
+LILY_TEST_CASE(test_c_compile_enum)

1ebc213f7b21944670188ef9510ec2129ccfd315
RobotMan2412 <julian@scheffers.net>
2026-05-13 00:30:47 +0200

    Include file demo

---
+++ test-files/preproc_1.h
@@ -0,0 +1,7 @@
+
+// THIS IS PREPROC 1
+// clang-format off
+
+#include "preproc_2.h"
+#include "preproc_2.h"
+#include "preproc_3.h"
---
+++ test-files/preproc_2.h
@@ -0,0 +1,6 @@
+
+// THIS IS PREPROC 2
+
+#pragma once
+
+#include "preproc_3.h"

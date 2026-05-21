33bbac081093b71424ea1130c925e41d51671157
RobotMan2412 <julian@scheffers.net>
2026-05-21 12:26:51 +0200

    Add malloc attributes

--- src/util/lilycc_malloc.h
+++ src/util/lilycc_malloc.h
@@ -16,12 +16,12 @@ extern atomic_size_t lilycc_total_alloc;
 #endif
 
 // Lily-CC malloc; abort if out of memory.
-void *lilycc_malloc(size_t size);
+void *lilycc_malloc(size_t size) __attribute__((malloc, warn_unused_result));
 // Lily-CC calloc; abort if out of memory.
-void *lilycc_calloc(size_t nmemb, size_t size);
+void *lilycc_calloc(size_t nmemb, size_t size) __attribute__((alloc_size(2, 1), warn_unused_result));
 // Lily-CC realloc; abort if out of memory.
-void *lilycc_realloc(void *ptr, size_t size);
+void *lilycc_realloc(void *ptr, size_t size) __attribute__((alloc_size(2), warn_unused_result));
 // Lily-CC stdup; abort if out of memory.
-char *lilycc_strdup(char const *str);
+char *lilycc_strdup(char const *str) __attribute__((warn_unused_result));
 // Lily-CC free.
 void  lilycc_free(void *mem);

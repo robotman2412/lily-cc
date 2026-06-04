84299497c66cf64074e565c726b023aea149a36c
Julian Scheffers <julian@scheffers.net>
2026-06-04 20:52:53 +0200

    Misc small fixes

--- src/compiler/front/c/c_tokenizer.c
+++ src/compiler/front/c/c_tokenizer.c
@@ -168,6 +168,7 @@ static token_t c_tkn_pre_number(tokenizer_t *ctx) {
 }
 
 // Convert preprocessing number token to C number token.
+// TODO: Convert to options, gate binary behind C23 || gnu extension flag, gate _x128 behind Lily-CC extension flag.
 token_t c_tkn_conv_number(cctx_t *cctx, int c_std, token_t const *pre_tkn) {
     i128_t val      = int128(0, 0);
     bool   hasdat   = false;
